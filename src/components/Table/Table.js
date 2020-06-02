049ef13c7394d64641fc06a6aa03f211f8b23e43
Amit Shah <as6383452@gmail.com>
2020-06-02 19:45:52 +0530

    before database call

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,21 +1,61 @@
-import React from 'react';
-import { BrowserRouter ,Router, Link } from 'react-router-dom'
-function Table() {
-  return (
-      <div>
-         <table border = "1">
-         <tr>
-            <td>Row 1, Column 1</td>
-            <td>Row 1, Column 2</td>
-         </tr>
-         
-         <tr>
-            <td>Row 2, Column 1</td>
-            <td>Row 2, Column 2</td>
-         </tr>
-      </table>
-      </div>      
-  );
+import React ,{ Component} from 'react';
+import './Table.css';
+import { BrowserRouter ,Router, Link } from 'react-router-dom';
+
+class Table extends Component {
+   state  = {
+      tablevalue : [
+         {name : 'amit', classs : '12th', section : 'B', rollno : 53, marks : 51, status: 'None'},
+         {name : 'amit', classs : '12th', section : 'B', rollno : 53, marks : 21, status: 'None'},
+         {name : 'amit', classs : '12th', section : 'B', rollno : 53, marks : 51, status: 'None'},
+         {name : 'amit', classs : '12th', section : 'B', rollno : 53, marks : 31, status: 'None'},
+         {name : 'amit', classs : '12th', section : 'B', rollno : 53, marks : 71, status: 'None'}
+      ]
+   };
+   statusHandler = () => {
+      const tabledata = this.state.tablevalue.slice();
+      tabledata.map((per) => {
+         if(per.marks < 50 ){
+            per.status = 'Fail';
+         }
+         else{
+            per.status = 'Pass';
+         }
+      });
+      this.setState({tablevalue : tabledata})
+   }
+  render() {     
+     let person = (                
+           this.state.tablevalue.map((per) => (
+            <tr>
+               <td>{per.name}</td>
+               <td>{per.classs}</td>
+               <td>{per.section}</td>
+               <td>{per.rollno}</td>
+               <td>{per.marks}</td>
+               <td>{per.status}</td>
+            </tr>            
+           ))               
+     );       
+     
+     return (
+         <div>
+            <table>
+               <tr>
+                  <th>Name</th>
+                  <th>Class</th>
+                  <th>Section</th>
+                  <th>Roll no</th>
+                  <th>Marks</th>
+                  <th>Status</th>
+               </tr>
+               {person}
+            </table>          
+            <button
+            onClick={this.statusHandler}>Get Status</button>  
+         </div>  
+     );    
+  }
 }
 
 export default Table;
